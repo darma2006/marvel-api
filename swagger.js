f86865cd7453ce08bff19a7e5575d0f9b7ffcf96
Darma Lopez <ldarma112@gmail.com>
2026-07-22 17:13:33 -0600

    week 4

--- swagger.js
+++ swagger.js
@@ -7,7 +7,15 @@ const doc = {
     version: "1.0.0"
   },
   host: "marvel-api-13cc.onrender.com",
-  schemes: ["https"]
+  schemes: ["https"],
+
+  securityDefinitions: {
+    googleAuth: {
+      type: "oauth2",
+      authorizationUrl: "/auth/google",
+      flow: "implicit"
+    }
+  }
 };
 
 const outputFile = "./swagger.json";
